2d0aed43b6eed2d3ebb850d5de46eac61c88eb20
WakisaC <wakisag@gmail.com>
2019-04-23 16:20:30 -0700

    first commit

---
+++ app/src/main/java/com/example/wakisac/myapplication/Launcher.java
@@ -0,0 +1,30 @@
+package com.example.wakisac.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Launcher extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_launcher);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+    }
+        public void getStarted(View view){
+            register();
+        }
+
+        public void register(){
+            Intent intent = new Intent(this,Register.class);
+            startActivity(intent);
+        }
+
+
+}
---
+++ app/src/main/java/com/example/wakisac/myapplication/Register.java
@@ -0,0 +1,38 @@
+package com.example.wakisac.myapplication;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+
+public class Register extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        toolbar.setNavigationIcon(R.drawable.back_arrow);
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
+
+    }
+
+    public void gotoMain(View view){
+        mainActivity();
+    }
+
+    public void mainActivity(){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
+
+
+}
